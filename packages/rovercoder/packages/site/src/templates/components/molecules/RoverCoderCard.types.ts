8f30341427810a5b442e87dda20bae049bc4a0d1
Nathan Zahra <nathanzahra@hotmail.com>
2025-11-11 11:19:13 +0100

    Refactored Text Component

---
+++ packages/rovercoder/packages/site/src/templates/components/molecules/RoverCoderCard.types.ts
@@ -0,0 +1,8 @@
+export interface ImageProps { 
+    url?: string | undefined; 
+    color?: string | undefined; 
+    imageRepeat?: string | undefined; 
+    imageSize?: string | undefined; 
+    description?: string | undefined;
+    isMainImage?: boolean | undefined;
+}
